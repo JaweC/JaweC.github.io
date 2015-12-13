f0c2e23b9042dd94d253e834a050c8903f7df0c3
Jawe <neus2benen@gmail.com>
2015-12-13 18:06:27 +0100

    tock

--- scripts/gifloop.js
+++ scripts/gifloop.js
@@ -1,16 +1,14 @@
-            var totalCount = 127;
-          function ChangeIt()
-          {
-         document.documentElement.style.overflow = 'hidden';  // firefox, chrome
-         document.body.scroll = "no"; // ie only
+var totalCount = 127;
+function ChangeIt() {
+  document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+  document.body.scroll = "no"; // ie only
          
-         //Unrelated to gif loop, this is easier to put in here
-          document.getElementById("music").volume = 0.1;
+  //Unrelated to gif loop, this is easier to put in here
+  document.getElementById("music").volume = 0.5;
 
-          var num = Math.floor(Math.random() * totalCount ) + 1;
-          var delay = Math.floor(Math.random() * 2500) + 1500;
+  var num = Math.floor(Math.random() * totalCount) + 1;
+  var delay = Math.floor(Math.random() * 2500) + 1500;
 
-          document.getElementById('parent').innerHTML = '<img src="data/media/images/summer/' + num + '.gif" />';
-
-          setTimeout(arguments.callee, delay);
-          }
+  document.getElementById('bg').innerHTML = '<img src="data/media/images/summer/' + num + '.gif" />';
+  setTimeout(arguments.callee, delay);
+}
